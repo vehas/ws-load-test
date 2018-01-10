6b9d815f4bd1e7f1671e631640626588fe0e7e8f
Veha Suwatphisankij <sky.48478@gmail.com>
2018-01-10 19:52:59 +0700

    lein new app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ws-load-test
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ws_load_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns ws-load-test.core-test
+  (:require [clojure.test :refer :all]
+            [ws-load-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
